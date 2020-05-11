6bb0c058186039237f1eccfcab9162153858916e
Corey Farrell <git@cfware.com>
2020-05-11 06:57:12 -0400

    Breaking: Update for Node.js 10 support
    * Enable parsing of ECMAScript 2018 syntax but forbid use by adding a
      eslint-plugin-node rule.
    * Permit use of ECMAScript 2018 global variables
    * Add common ignorePatterns into shared config
    * Declare override section for `test/**` to support mocha
    * Drop separate test.json published file
    * Declare support for node.js 10+

--- eslint.js
+++ eslint.js
@@ -1,12 +1,20 @@
 'use strict';
 
-var config = {
+const config = {
   "root": true,
   "extends": ["eslint:recommended", "prettier"],
+  "plugins": [
+      "node"
+  ],
+  "parserOptions": {
+    "ecmaVersion": 2018
+  },
   "env": {
-    "node": true
+    "node": true,
+    "es2017": true
   },
   "rules": {
+    "node/no-unsupported-features/es-syntax": [2, {"version": ">=0.10"}],
     "block-scoped-var": 2,
     "eqeqeq": [2, "smart"],
     "max-depth": [1, 3],
@@ -14,7 +22,23 @@ var config = {
     "new-cap": 1,
     "no-extend-native": 2,
     "no-unused-vars": 1,
-  }
+  },
+  "ignorePatterns": [
+    "coverage/**",
+    "test/fixtures/**"
+  ],
+  "overrides": [
+    {
+      "files": ["test/**"],
+      "env": {
+        "mocha": true
+      },
+      "rules": {
+        "max-len": 0,
+        "max-statements": 0
+      }
+    }
+  ]
 };
 
 module.exports = config;
