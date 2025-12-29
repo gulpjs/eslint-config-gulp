f605216df3ba1b9da74cc1c1da634a45bb525b55
Blaine Bublitz <blaine.bublitz@gmail.com>
2025-12-29 15:13:51 -0700

    feat!: Rewrite for eslint flat config

--- eslint.js
+++ eslint.js
@@ -1,50 +1,45 @@
-'use strict';
+var js = require("@eslint/js");
+var globals = require("globals");
 
-var config = {
-  "root": true,
-  "extends": ["eslint:recommended", "prettier"],
-  "plugins": [
-      "node"
-  ],
-  "parserOptions": {
-    "ecmaVersion": 2018
-  },
-  "env": {
-    "node": true,
-    "es2017": true
-  },
-  "rules": {
-    "node/no-unsupported-features/es-syntax": [
-      2,
-      {
-        "version": ">=0.10",
-        "ignores": ["classes"]
-      }
-    ],
-    "block-scoped-var": 2,
-    "eqeqeq": [2, "smart"],
-    "max-depth": [1, 3],
-    "max-statements": [1, 30],
-    "new-cap": 1,
-    "no-extend-native": 2,
-    "no-unused-vars": 1,
-  },
-  "ignorePatterns": [
-    "coverage/**",
-    "test/fixtures/**"
-  ],
-  "overrides": [
-    {
-      "files": ["test/**"],
-      "env": {
-        "mocha": true
+module.exports = [
+	js.configs.recommended,
+	{
+    languageOptions: {
+			ecmaVersion: 2018,
+			sourceType: "commonjs",
+			globals: {
+        ...globals.node,
+				...globals.es2017,
+			},
+		},
+		rules: {
+      "block-scoped-var": "error",
+      "eqeqeq": ["error", "smart"],
+      "max-depth": ["warn", { max: 3 }],
+      "max-statements": ["warn", { max: 30 }],
+      "new-cap": "warn",
+      "no-extend-native": "error",
+      "no-unused-vars": "warn",
+		},
+	},
+  {
+    files: ["test/**"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2017,
+        ...globals.mocha,
       },
-      "rules": {
-        "max-len": 0,
-        "max-statements": 0
-      }
+    },
+    rules: {
+      "max-len": "off",
+      "max-statements": "off"
     }
-  ]
-};
-
-module.exports = config;
+  },
+  {
+    ignores: [
+      "coverage/**",
+      "test/fixtures/**"
+    ]
+  }
+];
